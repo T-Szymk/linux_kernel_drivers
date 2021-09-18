e2eabbcc25f6fa833f734489b4873029d6e6ac73
tom_pi <ziggys_42@hotmail.com>
2021-09-18 21:46:44 +0100

    added test app for ioctl of hello_world char driver

---
+++ linux_rpi3b+/ioctl_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(void) {
+
+  /* First, you need to run 'mknod /dev/mydev c 202 0' to create /dev/mydev */
+  int my_dev = open("/dev/mydev", 0);
+
+  if (my_dev < 0) {
+    std::cerr << "Failed to open device file: /dev/mydev." << std::endl;
+  } else {
+    ioctl(my_dev, 100, 110); /* cmd = 100, arg = 110 */
+    close(my_dev);
+  }
+
+  return 0;
+}
